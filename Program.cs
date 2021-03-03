023569cd0dfeb77b41d12532e754dda734e51794
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-03-03 10:09:30 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current Date and Time is " + DateTime.Now);
+            Console.WriteLine("Please enter an number of hours to see what time it will be after those hours elapse");
+            double answer = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(answer));
+            Console.ReadLine();
+        }
+    }
+}
